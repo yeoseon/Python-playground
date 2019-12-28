e3a1692592fe460328a070042bc0024ab1c54401
Yeoseon Yun <devyyskr@gmail.com>
2019-12-28 16:44:25 +0900

    Hello world

--- DjangoPlayground/DjangoPlayground/urls.py
+++ DjangoPlayground/DjangoPlayground/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import helloworld.views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', helloworld.views.home, name='home'),
 ]
